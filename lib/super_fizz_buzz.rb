db52e2e4c76a92381d39d1bf726d071a40e02e1e
Alex Kiselich <alexander.kiselich@gmail.com>
2023-03-27 12:56:03 -0600

    Add code to solve problem

--- lib/super_fizz_buzz.rb
+++ lib/super_fizz_buzz.rb
@@ -1,2 +1,24 @@
 # Iterate through the numbers 1 to 1000
 # For each one, print the output according to the rules of Super Fizz Buzz
+numbers = [*1..1000]
+
+numbers.each do |number|
+  if number % 3 == 0 && number % 5 == 0 && number % 7 == 0
+    puts "SuperFizzBuzz"
+    elsif number % 3 == 0 && number % 7 == 0
+      puts "SuperFizz"
+        elsif number % 5 == 0 && number % 7 == 0
+          puts "SuperBuzz"
+            elsif number % 3 == 0 && number % 5 == 0
+              puts "FizzBuzz"
+                elsif number % 7 == 0
+                  puts "Super"
+                    elsif number % 5 == 0
+                      puts "Buzz"
+                        elsif number % 3 == 0
+                          puts "Fizz"
+                            else
+                              puts number 
+  end
+end
+  
